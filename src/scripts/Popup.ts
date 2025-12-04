34a47ab42e67a632337538c9c9682fd1bbdd49c1
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-12-04 13:24:41 +0200

    Add micromodal dependency and update styles and HTML structure

---
+++ src/scripts/Popup.ts
@@ -0,0 +1,33 @@
+import MicroModal from "micromodal";
+
+export default class Popup {
+  private selectors: Record<string, string> = {
+    trigger: "[data-js-hero-or a]",
+  };
+
+  private triggerElement: HTMLElement | null;
+
+  constructor() {
+    this.triggerElement = document.querySelector(this.selectors.trigger) as HTMLElement;
+
+    if (this.isReady()) {
+      this.init();
+    }
+  }
+
+  private isReady(): boolean {
+    return !!this.triggerElement;
+  }
+
+  private init(): void {
+    this.triggerElement?.addEventListener("click", this.handleClick.bind(this));
+  }
+
+  private handleClick(event: MouseEvent): void {
+    event.preventDefault();
+    MicroModal.show("hero-popup", {
+      disableScroll: true,
+      disableFocus: true,
+    });
+  }
+}
